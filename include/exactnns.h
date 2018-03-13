1cc78b8fa255220d32c1a8efd78249ad4e85735d
xinyandai <381675152@qq.com>
2018-03-13 16:42:17 +0800

    Fix bugssss and inject new bugs.

--- include/exactnns.h
+++ include/exactnns.h
@@ -54,13 +54,13 @@ public:
         lshboxFout << maxHeaps.size() << "\t" << topK_ << endl;
         for (int query_id = 0; query_id < maxHeaps.size(); ++query_id) {
             lshboxFout << query_id << "\t";
-            priority_queue<DistDataMax<Point<DataType> > >& maxHeap = maxHeaps[query_id];
+            priority_queue<DistDataMax<int > >& maxHeap = maxHeaps[query_id];
 
             while (maxHeap.size()>0) {
-                DistDataMax<Point<DataType>> kDist = maxHeap.top();
+                DistDataMax<int> kDist = maxHeap.top();
                 maxHeap.pop();
 
-                lshboxFout << kDist.data().getID() << "\t" << kDist.dist() << "\t";
+                lshboxFout << kDist.data() << "\t" << kDist.dist() << "\t";
             }
             lshboxFout << endl;
         }
@@ -68,62 +68,102 @@ public:
         cout << "lshbox groundtruth are written into " << lshboxBenchFileName << endl;
     }
 
+    void calculateQueryCenterDist(DataType* queryPoint, vector<vector<pair<size_t , DataType> > >& distToClusters) {
+        for (int codeBookID = 0; codeBookID < num_codebook_; ++codeBookID) {
+            distToClusters[codeBookID].reserve(codebook_subdimension[codeBookID]);
+        }
+
+        for (int codeBookID = 0; codeBookID < num_codebook_; ++codeBookID) {
+
+            KMeans<DataType>& means = kMeans[codeBookID];
+            vector<pair<size_t , DataType> >& dists = distToClusters[codeBookID];
+
+            for (int clusterId = 0; clusterId < means.getClusters().size(); ++clusterId) {
+                const Cluster<DataType>& cluster = means.getClusters()[clusterId];
+                DataType query_center = metric::euclidDistance(
+                        &queryPoint[codeBookID*codebook_subdim_max],
+                        cluster.getCentralValues().data(),
+                        codebook_subdimension[codeBookID]
+                );
+                DataType radius = cluster.getRadius();
+                DataType dist_lower_bound = query_center * query_center + radius * radius - 2 * query_center * radius;
+
+                dists.push_back(std::make_pair(clusterId, dist_lower_bound));
+            }
+
+            std::sort(
+                    dists.begin(),
+                    dists.end(),
+                    [](const pair<size_t , DataType>& a, const pair<size_t , DataType>& b) {
+                        return a.second < b.second;
+                    });
+        }
+    }
+
+    void searchOnePoint(DataType* queryPoint, priority_queue<DistDataMax<int > >& maxHeap) {
+
+        vector<vector<pair<size_t , DataType> > > distToClusters(num_codebook_);// dist from query to cluster center for each codebook and each center in each codebook
+
+        calculateQueryCenterDist(queryPoint, distToClusters);
+
+        auto distor = [&](vector<unsigned > clusterIDS) {
+
+            DataType dist_square = 0.0;
+            for (int codebookIndex = 0; codebookIndex < num_codebook_; ++codebookIndex) {
+
+                dist_square += distToClusters[codebookIndex][clusterIDS[codebookIndex]].second;
+            }
+
+            return dist_square;
+        };
+
+        IMISequence imiSequence(num_codebook_, vector<size_t >(num_codebook_, this->clusterK_), distor);
+
+        long double upperBound = std::numeric_limits<long double>::max();
+        long double lowerBound = - std::numeric_limits<long double>::max();
+
+        maxHeap.push( DistDataMax<int >(std::numeric_limits<DataType>::max(), 0 ) );
+
+        for (int bucketNum = 0; lowerBound<upperBound && imiSequence.hasNext(); bucketNum++) {
+            auto next = imiSequence.next();
+            unsigned long key = 0;
+            for (int codeBookIndex = num_codebook_-1; codeBookIndex >= 0; --codeBookIndex) {
+                unsigned clusterID = next.second[codeBookIndex];
+                pair<size_t , DataType>& dists = distToClusters[codeBookIndex][clusterID];
+                key = key * clusterK_ + dists.first;
+            }
+
+            if (tables.find(key) == tables.end()) {
+                assert(true);
+            }
+            const Cluster<DataType>& nextCluster = tables.find(key)->second;
+
+            DistDataMax<int > kDist = maxHeap.top();
+
+            for (int i = 0; i < nextCluster.getClusterSize(); ++i) {
+                DataType distance = metric::euclidDistance(data_[nextCluster.getPoint(i).getID()], queryPoint, (size_t)query_.getDim());
+
+                if (distance < kDist.dist()) {
+                    maxHeap.pop();
+                    maxHeap.push(DistDataMax<int >(distance, nextCluster.getPoint(i).getID()));
+                    kDist = maxHeap.top();
+                }
+            }
+
+            upperBound = metric::squareEuclidDistance(data_[kDist.data()], queryPoint, (size_t)query_.getDim());
+            lowerBound = next.first;
+
+        }
+
+    }
+
     void search() {
         // insert n empty
-        maxHeaps.insert(maxHeaps.end(), query_.getSize(), priority_queue<DistDataMax<Point<DataType> > > ());
+        maxHeaps.insert(maxHeaps.end(), query_.getSize(), priority_queue<DistDataMax<int > > ());
 
         for (int point_id = 0; point_id < query_.getSize(); ++point_id) {
 
-            DataType* queryPoint = query_[point_id];
-
-            auto distor = [&](vector<unsigned > clusterIDS) {
-
-                DataType dist_square = 0.0;
-                for (int codebookIndex = 0; codebookIndex < num_codebook_; ++codebookIndex) {
-
-                    KMeans<DataType>& currentKMeans = kMeans[codebookIndex];
-                    const Cluster<DataType>& cluster = currentKMeans.getClusters()[clusterIDS[codebookIndex]];
-
-                    DataType query_center = metric::euclidDistance(
-                            &queryPoint[codebookIndex*codebook_max_size],
-                            cluster.getCentralValues().data(),
-                            codebook_size[codebookIndex]
-                    );
-                    DataType radius = cluster.getRadius();
-                    DataType dist_lower_bound = query_center * query_center + radius * radius - 2 * query_center * radius;
-
-                    dist_square += dist_lower_bound;
-                }
-                return dist_square;
-            };
-            IMISequence imiSequence(num_codebook_, codebook_size, distor);
-
-            long double upperBound = std::numeric_limits<long double>::max();
-            long double lowerBound = - std::numeric_limits<long double>::max();
-            priority_queue<DistDataMax<Point<DataType> > >& maxHeap = maxHeaps[point_id];
-            maxHeap.push( DistDataMax<Point<DataType> >(std::numeric_limits<DataType>::max(), points[0][0] ) );
-
-            for (int bucketNum = 0; lowerBound<upperBound && imiSequence.hasNext(); bucketNum++) {
-                auto next = imiSequence.next();
-                const Cluster<DataType>& nextCluster = tables.find(lshbox::to_long(next.second, codebook_max_size))->second;
-
-                DistDataMax<Point<DataType>> kDist = maxHeap.top();
-
-                for (int i = 0; i < nextCluster.getClusterSize(); ++i) {
-                    DataType distance = metric::euclidDistance(nextCluster.getPoint(i).getValues(), queryPoint, (size_t)query_.getDim());
-
-                    if (distance < kDist.dist()) {
-                        maxHeap.pop();
-                        maxHeap.push(DistDataMax<Point<DataType> >(distance, nextCluster.getPoint(i)));
-                        kDist = maxHeap.top();
-                    }
-                }
-
-                upperBound = metric::squareEuclidDistance(data_[kDist.data().getID()], queryPoint, (size_t)query_.getDim());
-                lowerBound = next.first;
-
-            }
-
+            searchOnePoint(query_[point_id], maxHeaps[point_id]);
         }
     }
 
@@ -133,7 +173,7 @@ public:
 
         for (int i = 0; i < num_codebook_; ++i) {
 
-            kMeans.push_back( KMeans<DataType> (clusterK_, (size_t)data_.getSize(), codebook_size[i], max_iterations_, metric::squareEuclidDistance<DataType>) );
+            kMeans.push_back( KMeans<DataType> (clusterK_, (size_t)data_.getSize(), codebook_subdimension[i], max_iterations_, metric::squareEuclidDistance<DataType>) );
         }
 
 
@@ -143,7 +183,7 @@ public:
             subPoints.reserve((size_t)data_.getSize());
             for (int point_id = 0; point_id < data_.getSize(); ++point_id) {
 
-                subPoints.push_back(Point<DataType>(point_id, & data_[point_id][codebook_index * codebook_max_size]));
+                subPoints.push_back(Point<DataType>(point_id, & data_[point_id][codebook_index * codebook_subdim_max]));
             }
             points.push_back(subPoints);
         }
@@ -191,10 +231,10 @@ public:
     }
 
     void calculateCodeBook() {
-        codebook_max_size = data_.getDim() / num_codebook_;
-        codebook_size.insert(codebook_size.end(), num_codebook_, codebook_max_size);
+        codebook_subdim_max = data_.getDim() / num_codebook_;
+        codebook_subdimension.insert(codebook_subdimension.end(), num_codebook_, codebook_subdim_max);
         if (data_.getDim() % num_codebook_) {
-            codebook_size[num_codebook_-1] = data_.getDim() % num_codebook_;
+            codebook_subdimension[num_codebook_-1] = data_.getDim() % num_codebook_;
         }
     }
 
@@ -206,7 +246,7 @@ protected:
     std::unordered_map<unsigned long ,  Cluster<DataType> > tables;
 
     vector<KMeans<DataType> > kMeans;
-    vector<priority_queue<DistDataMax<Point<DataType> > > > maxHeaps;
+    vector<priority_queue<DistDataMax<int > > > maxHeaps;
     vector<vector<Point<DataType > > > points;
 
     size_t num_codebook_ ; // how much codebook
@@ -214,6 +254,6 @@ protected:
     size_t max_iterations_;
     int topK_;
 
-    size_t codebook_max_size;
-    vector<size_t > codebook_size;
+    size_t codebook_subdim_max;
+    vector<size_t > codebook_subdimension;
 };
