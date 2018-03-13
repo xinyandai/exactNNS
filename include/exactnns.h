5d69a8e1615bce98143703ea66dc208cbb6110ce
xinyandai <381675152@qq.com>
2018-03-13 20:15:43 +0800

    Fix bug: when query to center distance is no greater than radius, lower bound equals 0.

--- include/exactnns.h
+++ include/exactnns.h
@@ -1,5 +1,9 @@
 //
-// Created by xinyan on 3/13/18.
+//
+// Created by Xinyan Dai on 3/13/18.
+//
+// Exact Nearest Neighbor Search with The Inverted Multi Index
+//
 //
 
 #include <iostream>
@@ -11,9 +15,23 @@
 #include "metric.h"
 #include "imisequence.h"
 
+/**
+ * PQ: product quatization
+ * sub-KMeans: multiple sub-KMeans consist of one PQ by merging(joining)
+ * @tparam DataType
+ */
 template <typename DataType>
 class ExactNNS {
 public:
+    /**
+     *
+     * @param data
+     * @param query
+     * @param num_codebook
+     * @param clusterK number of cluster in each sub-KMeans
+     * @param topK
+     * @param max_iteration
+     */
     ExactNNS(lshbox::Matrix<DataType>& data,
              lshbox::Matrix<DataType>& query,
              size_t num_codebook, size_t clusterK, int topK, size_t max_iteration)
@@ -23,7 +41,7 @@ public:
               clusterK_(clusterK),
               max_iterations_(max_iteration),
               topK_(topK){
-        // 1.
+        // 1. calculate dimension in each code book
         calculateCodeBook();
         // 2. pre process
         preProcess();
@@ -31,18 +49,22 @@ public:
         // 2-dimension code, which means size of product quantization = 2
         buildIndex();
         // 4. merge sub-clusters
-        mergeProduct();
+        productJoinClusters();
         //query
         search();
         // write result
         writeResult();
     }
 
+    // TODO
     void preProcess() {
         // 2.1 mean
         // 2.2 eigen allocate or optimized product quantization
     }
 
+    /**
+     * write topK result into text file
+     */
     void writeResult() {
         string lshboxBenchFileName = "nn.lshbox";
         // lshbox file
@@ -72,6 +94,14 @@ public:
         cout << "lshbox groundtruth are written into " << lshboxBenchFileName << endl;
     }
 
+
+    /**
+     * calculate distance from query to cluster's center for each cluster in each code book
+     * distance = ||q-c||^2  + ||c-x||^2 - 2*||q-c||*||c-x||
+     *          = query_center * query_center + radius * radius - 2 * query_center * radius;
+     * @param queryPoint
+     * @param distToClusters
+     */
     void calculateQueryCenterDist(DataType* queryPoint, vector<vector<pair<size_t , DataType> > >& distToClusters) {
         for (int codeBookID = 0; codeBookID < num_codebook_; ++codeBookID) {
             distToClusters[codeBookID].reserve(codebook_subdimension[codeBookID]);
@@ -90,7 +120,12 @@ public:
                         codebook_subdimension[codeBookID]
                 );
                 DataType radius = cluster.getRadius();
-                DataType dist_lower_bound = query_center * query_center + radius * radius - 2 * query_center * radius;
+                DataType dist_lower_bound;
+                if (query_center > radius) {
+                    dist_lower_bound = (query_center - radius) * (query_center - radius);
+                } else {
+                    dist_lower_bound = 0;
+                }
 
                 dists.push_back(std::make_pair(clusterId, dist_lower_bound));
             }
@@ -104,10 +139,15 @@ public:
         }
     }
 
+    /**
+     * search topK nearest neighbor for queryPoint, and result is saved in maxHeap.
+     * @param queryPoint
+     * @param maxHeap
+     */
     void searchOnePoint(DataType* queryPoint, priority_queue<DistDataMax<int > >& maxHeap) {
 
-        vector<vector<pair<size_t , DataType> > > distToClusters(num_codebook_);// dist from query to cluster center for each codebook and each center in each codebook
-
+        // (cluster_id, distance) from query to cluster center for each codebook and each center in each codebook
+        vector<vector<pair<size_t , DataType> > > distToClusters(num_codebook_);
         calculateQueryCenterDist(queryPoint, distToClusters);
 
         auto distor = [&](vector<unsigned > clusterIDS) {
@@ -132,7 +172,7 @@ public:
         for (bucketNum = 0; imiSequence.hasNext() && lowerBound<upperBound; bucketNum++) {
             auto next = imiSequence.next();
             unsigned long key = 0;
-            for (int codeBookIndex = num_codebook_-1; codeBookIndex >= 0; --codeBookIndex) {
+            for (int codeBookIndex = 0; codeBookIndex < num_codebook_; ++codeBookIndex) {
                 unsigned clusterID = next.second[codeBookIndex];
                 pair<size_t , DataType>& dists = distToClusters[codeBookIndex][clusterID];
                 key = key * clusterK_ + dists.first;
@@ -166,6 +206,9 @@ public:
 
     }
 
+    /**
+     * search reult for each query point, and result is saved in maxHeaps.
+     */
     void search() {
         // insert n empty
         maxHeaps.insert(maxHeaps.end(), query_.getSize(), priority_queue<DistDataMax<int > > ());
@@ -176,70 +219,97 @@ public:
         }
     }
 
+    /**
+     * build multi index for each sub-code-book with sub-KMeans
+     */
     void buildIndex() {
 
         kMeans.reserve(num_codebook_);
 
+        // initialize sub-KMeans
         for (int i = 0; i < num_codebook_; ++i) {
 
-            kMeans.push_back( KMeans<DataType> (clusterK_, (size_t)data_.getSize(), codebook_subdimension[i], max_iterations_, metric::squareEuclidDistance<DataType>) );
+            kMeans.push_back( KMeans<DataType> (clusterK_, (size_t)data_.getSize(), codebook_subdimension[i], max_iterations_, metric::euclidDistance<DataType>) );
         }
 
-
-        for (int codebook_index = 0; codebook_index < num_codebook_; ++codebook_index) {
+        // wrap point for each sub-KMeans
+        for (int code_book_index = 0; code_book_index < num_codebook_; ++code_book_index) {
 
             vector<Point<DataType> > subPoints;
             subPoints.reserve((size_t)data_.getSize());
+
             for (int point_id = 0; point_id < data_.getSize(); ++point_id) {
 
-                subPoints.push_back(Point<DataType>(point_id, & data_[point_id][codebook_index * codebook_subdim_max]));
+                subPoints.push_back(Point<DataType>(point_id, & data_[point_id][code_book_index * codebook_subdim_max]));
             }
             points.push_back(subPoints);
         }
 
-
+        // building indexes
         for (int i = 0; i < num_codebook_; ++i) {
             KMeans<DataType>& means = kMeans[i];
-
+            // run KMeans
             means.run(points[i]);
+            // calculate radius in each sub-KMeans
             means.calculateRadius(metric::euclidDistance<DataType>);
         }
 
     }
 
-    void mergeRecursive(int codebook_index, const Cluster<DataType>& cluster) {
-        if (codebook_index == -1) {
-            std::cout << "cluster: " << cluster.getID() << " size: "<< cluster.getClusterSize()<< "\n";
-            for (int i = 0; i < cluster.getClusterSize(); ++i) {
-                std::cout << cluster.getPoint(i).getID() << "  " ;
+    /**
+     * join cluster for all combinations in all code book.
+     *
+     */
+    void productJoinClusters() {
+
+        /**
+         *  join function for two code book.
+         */
+        std::function<void (int, const Cluster<DataType>& )> mergeRecursive;
+
+        /**
+         * join function.
+         */
+        mergeRecursive = [&] (int codebook_index, const Cluster<DataType>& cluster) {
+
+            if (codebook_index == num_codebook_) {
+                // joined with "num_codebook_" clusters, put into tables because this is one kind of valid combination
+                // cluster.getID() = cluster[0] * clusterK_^(num_codebook_-1)
+                //                 + cluster[1] * clusterK_^(num_codebook_-2)
+                //                 ...
+                //                 + cluster[num_codebook_-1]
+                tables.emplace(std::make_pair(cluster.getID(), cluster));
+
+            } else {
+
+                KMeans<DataType>& subKMeans = kMeans[codebook_index];
+                for (int i = 0; i < clusterK_; ++i) {
+                    // joined with the codebook_index'th code book
+                    Cluster<DataType> mergedCluster = cluster.merge( subKMeans.getClusters()[i], clusterK_ );
+                    // then joined with the (codebook_index+1)'th code book
+                    mergeRecursive(codebook_index+1, mergedCluster );
+                }
             }
-            std::cout << "\n";
-            tables.emplace(std::make_pair(cluster.getID(), cluster));
-        } else {
+        };
 
-            KMeans<DataType>& subKMeans = kMeans[codebook_index];
-            for (int i = 0; i < clusterK_; ++i) {
-
-                Cluster<DataType> mergedCluster = cluster.merge( subKMeans.getClusters()[i], clusterK_ );
-                mergeRecursive(codebook_index-1, mergedCluster );
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    void mergeProduct() {
-
-        KMeans<DataType>& subKMeans = kMeans[num_codebook_-1];
+        // start with kMeans[0]
+        KMeans<DataType>& subKMeans = kMeans[0];
 
         for (int i = 0; i < subKMeans.getClusters().size(); ++i) {
 
-            const Cluster<DataType>& mergedCluster =  subKMeans.getClusters()[i];
-            mergeRecursive((int)num_codebook_-2, mergedCluster );
+            // initialize cluster with al clusters in kMeans[0],
+            const Cluster<DataType>& initialCluster =  subKMeans.getClusters()[i];
+            // then merge clusters in kMeans[1], then merge cluster in kMeans[2] ....
+            mergeRecursive(1, initialCluster );
         }
 
     }
 
+    /**
+     * calculate dimension of each code book
+     */
     void calculateCodeBook() {
+
         codebook_subdim_max = data_.getDim() / num_codebook_;
         codebook_subdimension.insert(codebook_subdimension.end(), num_codebook_, codebook_subdim_max);
         if (data_.getDim() % num_codebook_) {
@@ -252,17 +322,44 @@ protected:
     lshbox::Matrix<DataType>& data_;
     lshbox::Matrix<DataType>& query_;
 
+    /**
+     * key value table.
+     * key: merged(joined) cluster id.
+     * value: merged(joined) cluster.
+     */
     std::unordered_map<unsigned long ,  Cluster<DataType> > tables;
-
+    /**
+     * sub-KMeans collection for all code books.
+     */
     vector<KMeans<DataType> > kMeans;
+
+    /**
+     * result is save in max heaps according to the distance from each query point to each retrieved data.
+     */
     vector<priority_queue<DistDataMax<int > > > maxHeaps;
+
+    /**
+     * wrapped points for kMeans.
+     */
     vector<vector<Point<DataType > > > points;
 
-    size_t num_codebook_ ; // how much codebook
+    size_t num_codebook_ ;
+    /**
+     * number of clusters in each sub-KMeans
+     */
     size_t clusterK_ ;
+
     size_t max_iterations_;
+
     int topK_;
 
+    /**
+     * default dimension of code book except the last code book.
+     * the last code book is no larger than codebook_subdim_max(=data_.getDim()/num_codebook_)
+     */
     size_t codebook_subdim_max;
+    /**
+     * all dimensions
+     */
     vector<size_t > codebook_subdimension;
 };
